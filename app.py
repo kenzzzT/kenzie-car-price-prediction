18a3092b38dac61cc8625d4e0ac623ebe081fc5a
kenzzzT <kenz9036@gmail.com>
2025-12-03 15:54:00 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+import joblib
 
-st.title("Car Price Prediction App (Tanpa Pickle)")
 
-# Load dataset
-df = pd.read_csv("car_price_prediction_.csv")
+with open("model_linear.pkl", "rb") as file:
+model = joblib.load(file)
 
-# Pilih fitur
-X = df[['year', 'km_driven', 'mileage']]
-y = df['selling_price']
 
-# Train model langsung di Streamlit
-model = LinearRegression()
-model.fit(X, y)
+st.title("Car Price Prediction App (Linear Regression)")
 
-# Input user
+
+brand = st.selectbox("Brand", ["Tesla", "BMW", "Audi", "Ford"])
 year = st.number_input("Tahun Mobil", min_value=1990, max_value=2025, step=1)
-km_driven = st.number_input("Kilometer Tempuh", min_value=0)
-mileage = st.number_input("Milege (kmpl)", min_value=0.0, max_value=50.0, step=0.1)
+engine = st.number_input("Engine Size (L)", min_value=1.0, max_value=6.0, step=0.1)
+fuel = st.selectbox("Fuel Type", ["Petrol", "Diesel", "Electric"])
+trans = st.selectbox("Transmission", ["Manual", "Automatic"])
+mileage = st.number_input("Mileage (km)", min_value=0, step=100)
+condition = st.selectbox("Condition", ["New", "Used", "Like New"])
 
-# Prediksi
 if st.button("Prediksi Harga"):
-    features = [[year, km_driven, mileage]]
-    pred = model.predict(features)[0]
-    st.success(f"Harga Mobil Diprediksi: Rp {pred:,.0f}")
+    data = pd.DataFrame([{
+        "Brand": brand,
+        "Year": year,
+        "Engine Size": engine,
+        "Fuel Type": fuel,
+        "Transmission": trans,
+        "Mileage": mileage,
+        "Condition": condition
+    }])
+
+    result = model.predict(data)[0]
+
+    st.success(f" Prediksi Harga Mobil: **USD {result:,.2f}**")
+
