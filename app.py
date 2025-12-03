ca9c8e0a19ae51f95dd3ef4f0a1b8d0897afbbaf
kenzzzT <kenz9036@gmail.com>
2025-12-03 15:29:24 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pickle
+import numpy as np
+import pandas as pd
+
+
+model = pickle.load(open("model_linear.pkl", "rb"))
+
+st.title("Car Price Prediction App (Linear Regression)")
+
+
+brand = st.selectbox("Brand", ["Tesla", "BMW", "Audi", "Ford"])
+year = st.number_input("Tahun Mobil", min_value=1990, max_value=2025, step=1)
+engine = st.number_input("Engine Size (L)", min_value=1.0, max_value=6.0, step=0.1)
+fuel = st.selectbox("Fuel Type", ["Petrol", "Diesel", "Electric"])
+trans = st.selectbox("Transmission", ["Manual", "Automatic"])
+mileage = st.number_input("Mileage (km)", min_value=0, step=100)
+condition = st.selectbox("Condition", ["New", "Used", "Like New"])
+
+if st.button("Prediksi Harga"):
+    data = pd.DataFrame([{
+        "Brand": brand,
+        "Year": year,
+        "Engine Size": engine,
+        "Fuel Type": fuel,
+        "Transmission": trans,
+        "Mileage": mileage,
+        "Condition": condition
+    }])
+
+    result = model.predict(data)[0]
+
+    st.success(f" Prediksi Harga Mobil: **USD {result:,.2f}**")
+
